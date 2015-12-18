3b9fe6a11c65a4c1e3e43d353687d135b5e7dd0a
shivasheguri <shivasheguri@m-ize.com>
2015-12-18 17:29:07 +0530

    initial commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,24 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.io.InputStream;
 
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return showIndexPage();
     }
 
+    
+    private static Result showIndexPage(){
+    	try{
+			InputStream is=null;
+        	is = play.Play.application().resourceAsStream("/public/index.html");
+        	return ok(is).as("text/html");
+		}catch(Exception e){
+			return index();
+		}
+    }
 }
